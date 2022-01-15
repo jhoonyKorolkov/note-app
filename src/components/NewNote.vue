6b90db98457267e16c61de7c2630d3709c1bfd47
JhoonyKorolkov <webwork.jhoony@gmail.com>
2022-01-16 00:15:59 +0700

    first commit

---
+++ src/components/NewNote.vue
@@ -0,0 +1,50 @@
+<template>
+  <!-- new note -->
+  <div class="new-note">
+    <label>Title</label>
+    <input v-model="note.title" type="text" />
+    <label>Description</label>
+    <textarea v-model="note.descr"></textarea>
+
+    <label class="new-note__radio" v-for="(item, i) in note.priority" :key="i">
+      <input class="radio" type="radio" :value="item.name" v-model="note.status" />
+      <div class="custom">{{ item.value }}</div>
+    </label>
+
+    <button class="btn btnPrimary" @click="addNote">New note</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    note: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    addNote() {
+      this.$emit('addNote', this.note);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.new-note {
+  text-align: center;
+  &__radio {
+    & .radio {
+      display: none;
+    }
+    & .custom {
+      cursor: pointer;
+    }
+    & .radio:checked + .custom {
+      font-weight: bold;
+      color: #494ce8;
+    }
+  }
+}
+</style>
---
+++ src/components/Notes.vue
@@ -0,0 +1,150 @@
+<template>
+  <!-- note list -->
+  <div class="notes">
+    <div
+      class="note"
+      :class="{ full: !grid, high: note.status == 'high', medium: note.status == 'medium' }"
+      v-for="note in notes"
+      :key="note.id"
+    >
+      <div class="note-header" :class="{ full: !grid }">
+        <p v-if="!note.editor" @click="showEditor(note)">
+          {{ note.title }}
+        </p>
+
+        <input
+          v-else
+          type="text"
+          v-model="note.title"
+          @change="changeTitle(note)"
+          @keydown.esc="returnTitle(note.id)"
+        />
+        <p style="cursor: pointer" @click="removeNote(note.id)">x</p>
+        {{ note.id }}
+      </div>
+      <div class="note-body">
+        <p>{{ note.descr }}</p>
+        <span>{{ note.date }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    notes: {
+      type: Array,
+      required: true,
+    },
+    grid: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      saveTitle: '',
+      hidden: false,
+    };
+  },
+  methods: {
+    removeNote(id) {
+      this.$emit('remove', id);
+    },
+
+    showEditor(note) {
+      this.saveTitle = note.title;
+      note.editor = true;
+    },
+
+    changeTitle(note) {
+      // confirm('Вы точно хотите сохранить заголовок?');
+      this.saveTitle = '';
+      note.editor = false;
+    },
+
+    returnTitle(id) {
+      confirm('Вы точно хотите отменить редактирование?');
+      const note = this.notes.find((item) => item.id == id);
+      console.log(note);
+      note.title = this.saveTitle;
+
+      note.editor = false;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.notes {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  padding: 40px 0;
+}
+.note {
+  width: 48%;
+  padding: 18px 20px;
+  margin-bottom: 20px;
+  background-color: #ffffff;
+  transition: all 0.25s cubic-bezier(0.02, 0.01, 0.47, 1);
+  box-shadow: 0 30px 30px rgba(0, 0, 0, 0.02);
+  &:hover {
+    box-shadow: 0 30px 30px rgba(0, 0, 0, 0.04);
+    transform: translate(0, -6px);
+    transition-delay: 0s !important;
+  }
+  &.full {
+    width: 100%;
+    text-align: center;
+  }
+  &.medium {
+    background: yellow;
+  }
+  &.high {
+    background: red;
+  }
+}
+.note-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  h1 {
+    font-size: 32px;
+  }
+  p {
+    font-size: 22px;
+    color: #402caf;
+  }
+  svg {
+    margin-right: 12px;
+    color: #999999;
+    &.active {
+      color: #402caf;
+    }
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+  &.full {
+    justify-content: center;
+    p {
+      margin-right: 16px;
+      &:last-child {
+        margin-right: 0;
+      }
+    }
+  }
+}
+.note-body {
+  p {
+    margin: 20px 0;
+  }
+  span {
+    font-size: 14px;
+    color: #999999;
+  }
+}
+</style>
